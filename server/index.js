4993ec6a5744ea3c7ea480636636a7e5063e4422
najm <n0jm202@gmail.com>
2023-06-22 19:48:45 -0400

    deploying

--- server/index.js
+++ server/index.js
@@ -61,12 +61,12 @@ app.use('/posts', postRoutes);
 
 /* Deployment */
 // Serve static files from the React app
-
 app.use(express.static(path.join(__dirname, '../client/build')));
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
-});
 
+// The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
 mongoose
